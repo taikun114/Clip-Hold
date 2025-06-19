d92332a748ef7b98b44521f8be6f5a4ed9ddf30c
たいくん <98800458+taikun114@users.noreply.github.com>
2025-06-19 16:58:26 +0900

    Initial Commit

---
+++ Clip Hold/Clip_HoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clip_HoldApp.swift
+//  Clip Hold
+//
+//  Created by 今浦大雅 on 2025/06/19.
+//
+
+import SwiftUI
+
+@main
+struct Clip_HoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clip HoldTests/Clip_HoldTests.swift
@@ -0,0 +1,17 @@
+//
+//  Clip_HoldTests.swift
+//  Clip HoldTests
+//
+//  Created by 今浦大雅 on 2025/06/19.
+//
+
+import Testing
+@testable import Clip_Hold
+
+struct Clip_HoldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
