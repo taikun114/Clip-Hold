cdba376e6290863e50629072caf015a7a0487a42
たいくん <98800458+taikun114@users.noreply.github.com>
2025-09-18 19:55:52 +0900

    Update KeyboardShortcuts

--- Clip Hold/LicenseInfoModalView.swift
+++ Clip Hold/LicenseInfoModalView.swift
@@ -31,7 +31,7 @@ struct LicenseInfoModalView: View {
     private let qwenCodeVersionString: String = "0.0.10"
 
     // KeyboardShortcutsの使用バージョン
-    private let keyboardShortcutsVersionString: String = "2.3.0"
+    private let keyboardShortcutsVersionString: String = "2.4.0"
     
     // create-dmgの使用バージョン
     private let createDmgVersionString: String = "1.2.2"
